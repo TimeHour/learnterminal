a39156bd3d4db8589991a3d49a279f66b8193c66
Martin Luberg <martin.luberg@tptlive.ee>
2022-11-08 14:08:33 +0200

    add more question

--- questions.js
+++ questions.js
@@ -12,8 +12,9 @@ inquirer
                 {type:'number', name:'age', message: 'How old are you?'},      
                 {type:'input', name:'name', message: 'What is your name?'},
                 {type:'input', name:'eye', message: 'What is your eye color'},
-                {type:'input', name:'eye', message: 'What is your eye color'},
                 {type:'input', name:'food', message: 'What is your favorite food?'},
+                {type:'expand', name:'gender', message: 'What is your gender', choices:["Male","Female","Other"]},
+                
 
 
 
@@ -24,7 +25,17 @@ inquirer
                  console.log(`My name is ${answer.name}`);
                  console.log(`My eye color is ${answer.eye}`);
                  console.log(`My favorite food is ${answer.food}`);
-  
+                 console.log(`My gender is ${answer.gender}`);
+                  console.log(`alright thanks for your identity ima go and make u go to jail`);
+
+                  
+inquirer
+    .prompt([
+                {type:'confirm', name:'yes', message: 'Is this right?'},
+])
+.then((answer)=> {
+                  console.log(`alright thanks for your identity ima go and make u go to jail`);  
+})
 
 
     
